1f675ce32b6c28563bb2fe745575cdedc15af965
sory92 <sorybana92@gmail.com>
2021-08-12 10:04:16 +0200

    Project Commit

---
+++ src/test/java/com/bana/studentmanager/StudentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bana.studentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
